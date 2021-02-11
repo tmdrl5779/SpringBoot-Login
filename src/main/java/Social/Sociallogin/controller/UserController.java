815f458b915f67a2f12bc8c947ea21172e23ce56
tmdrl5779 <tmdrl5779@gmail.com>
2021-02-12 00:52:15 +0900

    [ADD] 메인페이지, 로그인, 회원가입 페이지 init

--- src/main/java/Social/Sociallogin/controller/UserController.java
+++ src/main/java/Social/Sociallogin/controller/UserController.java
@@ -1,7 +1,19 @@
 package Social.Sociallogin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
+
+    @GetMapping("/user/joinForm")
+    public String joinForm(){
+        return "user/joinForm";
+    }
+
+    @GetMapping("/user/loginForm")
+    public String loginForm(){
+        return "user/loginForm";
+    }
 }
