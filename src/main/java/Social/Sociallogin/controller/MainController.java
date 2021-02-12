d991c0d1683e2133758a95e543546a4448dd6f08
tmdrl5779 <tmdrl5779@gmail.com>
2021-02-13 00:32:51 +0900

    [ADD] 스프링 시큐리티 로그인 구현

--- src/main/java/Social/Sociallogin/controller/MainController.java
+++ src/main/java/Social/Sociallogin/controller/MainController.java
@@ -1,12 +1,15 @@
 package Social.Sociallogin.controller;
 
+import Social.Sociallogin.config.auth.PrincipalDetail;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
     @RequestMapping("/")
-    public String mainPage(){
+    public String mainPage(@AuthenticationPrincipal PrincipalDetail principalDetail){// 컨트롤러에서 세션을 어떻게 찾는지??
+        //System.out.println("로그인 사용자 아이디:" + principalDetail.getUsername());
         return "main";
     }
 }
